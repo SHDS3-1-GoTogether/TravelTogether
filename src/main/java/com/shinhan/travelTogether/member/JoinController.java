12f361d306087fb4d90ef7c409d54edb5e1431f4
syc98syc <helloworldnewstart98@gmail.com>
2024-06-18 18:42:39 +0900

    Remove: Remove target

--- src/main/java/com/shinhan/travelTogether/member/JoinController.java
+++ src/main/java/com/shinhan/travelTogether/member/JoinController.java
@@ -14,11 +14,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
+ 
 @Controller
 @RequestMapping("/auth")
 public class JoinController {
 
+	
 	@Autowired
 	MemberService mService;
 
