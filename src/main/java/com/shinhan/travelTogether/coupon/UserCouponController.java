8dd45240717ddaf9850fe20a6464ae2a72892271
wjdals898 <jungmin898@naver.com>
2024-06-20 18:05:11 +0900

    Feat: Add mypage coupon list of login user

--- src/main/java/com/shinhan/travelTogether/coupon/UserCouponController.java
+++ src/main/java/com/shinhan/travelTogether/coupon/UserCouponController.java
@@ -2,6 +2,8 @@ package com.shinhan.travelTogether.coupon;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.shinhan.travelTogether.member.MemberDTO;
 
 @Controller
 @RequestMapping("/mypage")
@@ -21,10 +25,10 @@ public class UserCouponController {
 	Logger logger = LoggerFactory.getLogger(UserCouponController.class);
 	
 	@GetMapping("/couponList.do")
-	public void userCouponList(Model model){
+	public void userCouponList(Model model, HttpSession session){
 		
 		// 로그인 기능 구현시 수정
-		int userId = 1;
+		int userId = ((MemberDTO) session.getAttribute("member")).getMember_id();
 		List<UserCouponDTO> couponlist = userCouponService.selectAllUserCoupon(userId);
 		System.out.println(couponlist.toString());
 		logger.info(couponlist.size()+"건 조회됨");
